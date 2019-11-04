c180691fd2ae54459184d723f5bd9a761311968d
YezPed <chirasit.p@gmail.com>
2019-11-04 14:07:54 +0700

    Design UI home Success

---
+++ lib/screens/home.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  // Explicit
+
+  // Method
+  Widget authenButton() {
+    return RaisedButton(
+      color: Colors.orange[800],
+      child: Text(
+        'Authentication',
+        style: TextStyle(color: Colors.white),
+      ),
+      onPressed: () {},
+    );
+  }
+
+  Widget showLogo() {
+    return Container(
+      width: 120.0,
+      height: 120.0,
+      child: Image.asset('images/logo.png'),
+    );
+  }
+
+  Widget showAppName() {
+    return Text(
+      'Fuck you',
+      style: TextStyle(
+        color: Colors.red.shade900,
+        fontSize: 30.0,
+        fontWeight: FontWeight.bold,
+        fontStyle: FontStyle.italic,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: RadialGradient(
+              colors: [Colors.white, Colors.black],
+              radius: 1.0,
+            ),
+          ),
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                showLogo(),
+                showAppName(),
+                authenButton(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
